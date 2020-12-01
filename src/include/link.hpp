29463ee7fca62236439d6620f93df9005167378e
Adrian Dominguez <adrian.dominguez1@ibm.com>
2020-12-01 12:26:23 -0600

    lllinkedlist

--- src/include/link.hpp
+++ src/include/link.hpp
@@ -6,4 +6,37 @@
 #ifndef LINK_HPP
 #define LINK_HPP
 
+template <class dataType>
+class Link
+{
+private:
+    class Node
+    {
+    private:
+        Node *next;
+        Node *prev;
+        dataType data;
+    };
+
+    int size;
+    Node *head;
+
+public:
+    Link();
+    Link(const Link &aList);
+    Link(Node *head);
+
+    ~Link();
+
+    bool isEmpty();
+    int getLength();
+    void add(int index, dataType data);
+    void remove(int index);
+    dataType retrieve(int index);
+    dataType extract(int index);
+    void printLinkedList();
+    void removeDuplicates();
+    Node *find(int index);
+};
+
 #endif
