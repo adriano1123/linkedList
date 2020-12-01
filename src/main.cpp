9a2efb31cdf2fac4a03649ffee8258278f6a7b27
Adrian Dominguez <adrian.dominguez1@ibm.com>
2020-12-01 12:26:00 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,20 @@
 */
 
 #include <iostream>
+#include "include/link.hpp"
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	Link<int> list; 
+	list.add(1,5); 
+	list.add(2,3); 
+	list.add(5,4); 
+
+	list.printLinkedList(); 
+
+	Link<bool> listbooll; 
+	listbooll.add(1, true); 
+	listbooll.add(2,false); 
+
+	listbooll.printLinkedList();
 }
